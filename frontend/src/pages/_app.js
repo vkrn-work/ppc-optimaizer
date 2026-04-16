06097b1638b402d367b3c96a8dd6c59774a6ebcd
vkrn-work <vadim.a.koren@gmail.com>
2026-04-17 00:57:13 +0300

    Update _app.js

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,10 +1,5 @@
 import '../styles/globals.css'
-import { AccountProvider } from '../hooks/useAccount'
 
 export default function App({ Component, pageProps }) {
-  return (
-    <AccountProvider>
-      <Component {...pageProps} />
-    </AccountProvider>
-  )
+  return <Component {...pageProps} />
 }
